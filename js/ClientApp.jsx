547e47a198bd78c9f1fa1c31a15e0d53da33d4ee
Rob Kreisel <rob@design1010.com>
2018-01-17 12:54:33 -0600

    lint

---
+++ js/ClientApp.jsx
@@ -0,0 +1,18 @@
+const ce = React.createElement;
+
+const MyTitle = function(props) {
+  return ce('div', null, ce('h1', { style: { color: props.color } }, props.title));
+};
+
+const MyFirstComponent = function() {
+  return ce(
+    'div',
+    { id: 'my-first-component' },
+    ce(MyTitle, { title: 'Game of Thrones', color: 'GreenYellow' }),
+    ce(MyTitle, { title: 'Stranger Things', color: 'LimeGreen' }),
+    ce(MyTitle, { title: 'Rick and Morty', color: 'peru' }),
+    ce(MyTitle, { title: 'Silicon Valley', color: 'YellowGreen' })
+  );
+};
+
+ReactDOM.render(ce(MyFirstComponent), document.getElementById('app'));
