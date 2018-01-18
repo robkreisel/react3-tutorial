39c0316940d418a14a789db83ab65fa1551b38cc
Rob Kreisel <rob@design1010.com>
2018-01-18 14:07:47 -0600

    Refactored using JSX

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,21 +1,23 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-const ce = React.createElement;
-
 const MyTitle = function(props) {
-  return ce('div', null, ce('h1', { style: { color: props.color } }, props.title));
-};
-
-const MyFirstComponent = function() {
-  return ce(
-    'div',
-    { id: 'my-first-component' },
-    ce(MyTitle, { title: 'Game of Thrones', color: 'GreenYellow' }),
-    ce(MyTitle, { title: 'Stranger Things', color: 'LimeGreen' }),
-    ce(MyTitle, { title: 'The Americans', color: 'peru' }),
-    ce(MyTitle, { title: 'Silicon Valley', color: 'YellowGreen' })
+  return (
+    <div>
+      <h1 style={{ color: props.color }}>{props.title}</h1>
+    </div>
   );
 };
 
-render(ce(MyFirstComponent), document.getElementById('app'));
+const MyFirstComponent = function() {
+  return (
+    <div id="my-first-_component">
+      <MyTitle title="Game of Thrones" color="YellowGreen" />
+      <MyTitle title="Stranger Things" color="GreenYellow" />
+      <MyTitle title="House of Cards" color="Peru" />
+      <MyTitle title="The Americans" color="burlywood" />
+    </div>
+  );
+};
+
+render(<MyFirstComponent />, document.getElementById('app'));
