96669fcc097c726e98e97ccbf190e5d1af3379a2
Rob Kreisel <rob@design1010.com>
2018-01-19 16:09:16 -0600

    Transform class properties

--- js/Search.jsx
+++ js/Search.jsx
@@ -3,24 +3,23 @@ import ShowCard from './ShowCard';
 import preload from '../data.json';
 
 class Search extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      searchTerm: 'this is some sort of debug statement'
-    };
-
-    this.handleSearchTermChange = this.handleSearchTermChange.bind(this);
-  }
-  handleSearchTermChange (event) {
-      this.setState({searchTerm: event.target.value})
-  }
+  state = {
+    searchTerm: ''
+  };
+  handleSearchTermChange = event => {
+    this.setState({ searchTerm: event.target.value });
+  };
   render() {
     return (
       <div className="search">
         <header>
           <h1>svideo</h1>
-          <input onChange={this.handleSearchTermChange} value={this.state.searchTerm} type="text" placeholder="Search" />
+          <input
+            onChange={this.handleSearchTermChange}
+            value={this.state.searchTerm}
+            type="text"
+            placeholder="Search"
+          />
         </header>
         <div>
           {preload.shows.map(show => <ShowCard key={show.imdbID} {...show} />)}
