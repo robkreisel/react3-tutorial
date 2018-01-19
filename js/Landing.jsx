8f53856adcb9f11f2a94726c2f6a0b646d201bf3
Rob Kreisel <rob@design1010.com>
2018-01-19 11:22:59 -0600

    Added additional pages and routing

--- js/Landing.jsx
+++ js/Landing.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Landing = () => (
-<div className='landing'>
+  <div className="landing">
     <h1>svideo</h1>
-    <input type="text" placeholder='Search' />
-    <a>or Browse All</a>
-</div>
-
+    <input type="text" placeholder="Search" />
+    <Link to="/search">or Browse All</Link>
+  </div>
 );
 
 export default Landing;
