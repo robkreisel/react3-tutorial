bb0b4bb8c914fbaa27eb5523f69896e0b27d51a5
Rob Kreisel <rob@design1010.com>
2018-01-19 12:35:36 -0600

    Styled components

--- js/Search.jsx
+++ js/Search.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import ShowCard from './ShowCard'
+import ShowCard from './ShowCard';
 import preload from '../data.json';
 
 const Search = () => (
   <div className="search">
-    {preload.shows.map(show => <ShowCard key={show.imdbID} {...show} />)}
+    <div>
+      {preload.shows.map(show => <ShowCard key={show.imdbID} {...show} />)}
+    </div>
   </div>
 );
 
