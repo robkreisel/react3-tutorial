f84c87e18f385bfe335a6f204ecfae3e99723779
Rob Kreisel <rob@design1010.com>
2018-01-22 16:12:00 -0600

    Testing

--- js/Search.jsx
+++ js/Search.jsx
@@ -3,7 +3,7 @@ import ShowCard from './ShowCard';
 import preload from '../data.json';
 
 class Search extends Component {
-   state = {
+  state = {
     searchTerm: ''
   };
   handleSearchTermChange = event => {
@@ -23,13 +23,11 @@ class Search extends Component {
         </header>
         <div>
           {preload.shows
-              .filter(
-                  show =>
-                      `${show.title} ${show.description}`
-                  .toUpperCase()
-                  .indexOf(this.state.searchTerm.toUpperCase()) >= 0
-              )
-              .map(show => <ShowCard key={show.imdbID} {...show} />)}
+            .filter(
+              show =>
+                `${show.title} ${show.description}`.toUpperCase().indexOf(this.state.searchTerm.toUpperCase()) >= 0
+            )
+            .map(show => <ShowCard key={show.imdbID} {...show} />)}
         </div>
       </div>
     );
