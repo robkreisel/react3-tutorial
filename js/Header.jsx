280ffab9578679f55cae35241e2f8771796133f5
Rob Kreisel <rob@design1010.com>
2018-01-25 08:33:05 -0600

    Conditional display logic; but problem with imdbID

--- js/Header.jsx
+++ js/Header.jsx
@@ -3,33 +3,37 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Header = (props: { showSearch?: boolean}) => {
-    let utilSpace
-    if (props.showSearch) {
-        utilSpace = <h1>lol</h1>
-    } else {
-        utilSpace = (
-            <h2>
-                <Link to={"/search"}>
-                    Back
-                </Link>
-            </h2>
-        );
-    }
-    return (
-        <header>
-            <h1>
-                <Link to="/">
-                    svideo
-                </Link>
-            </h1>
-            {utilSpace}
-        </header>
+const Header = (props: { showSearch?: boolean, handleSearchTermChange?: Function, searchTerm?: string }) => {
+  let utilSpace;
+  if (props.showSearch) {
+    utilSpace = (
+      <input onChange={props.handleSearchTermChange} value={props.searchTerm} type="text" placeholder="Search" />
     );
+  } else {
+    utilSpace = (
+      <h2>
+        <Link to={'/search'}>
+          Back
+        </Link>
+      </h2>
+    );
+  }
+  return (
+    <header>
+      <h1>
+        <Link to="/">
+          svideo
+        </Link>
+      </h1>
+      {utilSpace}
+    </header>
+  );
 };
 
 Header.defaultProps = {
-    showSearch: false
+  showSearch: false,
+  handleSearchTermChange: function noop() {},
+  searchTerm: ''
 };
 
 export default Header;
