cbe85f57b9c0d91c21ebc7ea9951dbed6a0b1794
Rob Kreisel <rob@design1010.com>
2018-01-29 09:20:57 -0600

    Refactor Reducers

--- js/Landing.jsx
+++ js/Landing.jsx
@@ -1,6 +1,6 @@
 // @flow
 
-import React from 'react';
+import React, { Component} from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import type { RouterHistory } from 'react-router-dom';
